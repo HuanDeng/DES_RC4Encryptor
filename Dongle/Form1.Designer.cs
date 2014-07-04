a5b75b7271749a6f276c3b1667b7e80ea0b6a856
HuanDeng <huan_d@sina.com>
2014-07-04 15:04:40 +0800

    新建项目

---
+++ Dongle/Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace Dongle
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.encrypt_b = new System.Windows.Forms.Button();
+            this.decrypt_b = new System.Windows.Forms.Button();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // encrypt_b
+            // 
+            this.encrypt_b.Location = new System.Drawing.Point(12, 12);
+            this.encrypt_b.Name = "encrypt_b";
+            this.encrypt_b.Size = new System.Drawing.Size(166, 56);
+            this.encrypt_b.TabIndex = 0;
+            this.encrypt_b.Text = "Encrypt";
+            this.encrypt_b.UseVisualStyleBackColor = true;
+            this.encrypt_b.Click += new System.EventHandler(this.encrypt_b_Click);
+            // 
+            // decrypt_b
+            // 
+            this.decrypt_b.AllowDrop = true;
+            this.decrypt_b.Location = new System.Drawing.Point(12, 81);
+            this.decrypt_b.Name = "decrypt_b";
+            this.decrypt_b.Size = new System.Drawing.Size(166, 56);
+            this.decrypt_b.TabIndex = 1;
+            this.decrypt_b.Text = "Decrypt";
+            this.decrypt_b.UseVisualStyleBackColor = true;
+            this.decrypt_b.Click += new System.EventHandler(this.decrypt_b_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(190, 149);
+            this.Controls.Add(this.decrypt_b);
+            this.Controls.Add(this.encrypt_b);
+            this.Name = "Form1";
+            this.Text = "Dongle";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button encrypt_b;
+        private System.Windows.Forms.Button decrypt_b;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+    }
+}
+
---
+++ Dongle/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+using System.IO;
+
+namespace Dongle
+{
+    public partial class Form1 : Form
+    {
+        private string Key = "pyxis=1$";
+        private string Rc4Key = "pyxis2013";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        EncryptSystem es = new EncryptSystem();
+        private void encrypt_b_Click(object sender, EventArgs e)
+        {
+            openFileDialog.Title = "Please select a .txt file";
+            //openFileDialog.Filter = "txt(*.txt)|*.txt";
+            //openFileDialog.FileName = "Json";
+            if (openFileDialog.ShowDialog() == DialogResult.OK) 
+            {
+
+                string fileBytes = File.ReadAllText (openFileDialog.FileName);
+                string newfileBytes = es.Encrypt(fileBytes, Key);
+                string filePath = Path.GetDirectoryName(openFileDialog.FileName) +"\\"+ Path.GetFileNameWithoutExtension (openFileDialog.FileName)+"Encrypt" + ".db";
+                File.WriteAllText(filePath, newfileBytes);
+            }
+        }
+        private void decrypt_b_Click(object sender, EventArgs e)
+        {
+            openFileDialog.Title = "Please select a .fir file";
+            openFileDialog.Filter = "fir(*.fir)|*.fir";
+            openFileDialog.FileName = "fir";
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+               // byte[] fileByte = File.ReadAllBytes(openFileDialog.FileName);
+               // //byte[] results=Rc4Crpter
+               //// string result = System.Text.Encoding.ASCII.GetString(fileByte);
+               // //string binggo = Rc4Crpter.Decrypt(fileByte, "pyxis2013", CryptoBase.EncoderMode.HexEncoder);
+               // byte[] binggo = Rc4Crpter1.DecryptEx(fileByte, "pyxis2013");
+               // string pp = System.Text.Encoding.Default.GetString(binggo);
+
+                string fileBytes = File.ReadAllText(openFileDialog.FileName);
+                string newfileBytes = es.Decrypt(fileBytes, Key);
+                string filePath = Path.GetDirectoryName(openFileDialog.FileName) + "\\" + Path.GetFileNameWithoutExtension(openFileDialog.FileName) + "Decrypt" + ".txt";
+                File.WriteAllText(filePath, newfileBytes);
+                string[] ds = File.ReadAllLines(filePath);
+                
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+
+}
